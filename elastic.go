d5b3091b27c140d6a76f766881103531b9f5e058
Gabriel Nelle <nelle@raceresult.com>
2017-06-28 15:04:44 +0200

    initial commit

---
+++ elastic.go
@@ -0,0 +1,184 @@
+package elasticsearch
+
+import (
+	"context"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"os"
+	"strings"
+
+	"gopkg.in/olivere/elastic.v5"
+)
+
+var (
+	clients = map[string]*client{}
+	urls    = map[string]string{}
+)
+
+func RegisterClient(name, url string) {
+	urls[name] = url
+}
+
+func newClient(name string) *client {
+	conn, ok := clients[name]
+	if !ok {
+		url, ok := urls[name]
+		if !ok {
+			log.Fatal(fmt.Sprintf("unknown elasticsearch client %s", name))
+		}
+
+		conn = &client{name: name, url: url}
+		clients[name] = conn
+		conn.checkConn()
+	}
+
+	return conn
+}
+
+type client struct {
+	name string
+	url  string
+	conn *elastic.Client
+}
+
+func (s *client) checkConn() error {
+	var err error
+	if s.conn == nil {
+		err = s.newConn()
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
+	return err
+}
+
+func (s *client) newConn() error {
+	log.Printf("Opening new Elastic connection to %s called '%s'", s.url, s.name)
+	cl, err := elastic.NewSimpleClient(elastic.SetURL(s.url),
+		elastic.SetErrorLog(log.New(os.Stderr, "ELASTIC ", log.LstdFlags)),
+		elastic.SetInfoLog(log.New(ioutil.Discard, "", log.LstdFlags)))
+	s.conn = cl
+	return err
+}
+
+func NewIndex(name, db string) *Index {
+	cl := newClient(db)
+	return &Index{cl: cl, name: name}
+}
+
+type Index struct {
+	cl       *client
+	name     string
+	settings map[string]string
+	docTypes []*DocType
+}
+
+func (s *Index) CheckStructure() error {
+	exists, err := s.indexExists(s.name)
+	if err == nil && !exists {
+		err = s.CreateIndex(s.name)
+	}
+	if err != nil {
+		log.Fatal(err)
+	}
+	return err
+}
+
+func (s *Index) indexExists(index string) (bool, error) {
+	return s.cl.conn.IndexExists(index).Do(context.TODO())
+}
+
+// CreateIndex creates an index by name. The index specified in the struct is created anyway if it doesnt exist.
+func (s *Index) CreateIndex(index string) error {
+	if s.settings == nil {
+		s.settings = map[string]string{}
+	}
+
+	mappings := map[string]string{}
+	for _, docType := range s.docTypes {
+		if docType.mapping != "" {
+			mappings[docType.name] = docType.mapping
+		}
+	}
+	body := fmt.Sprintf(`{"settings": %s, "mappings": %s}`,
+		strings.Trim(fmt.Sprintf("%#v", s.settings), "map[string]"),
+		strings.Trim(fmt.Sprintf("%#v", mappings), "map[string]"))
+
+	createIndex, err := s.cl.conn.CreateIndex(index).Body(body).Do(context.TODO())
+	if err == nil && !createIndex.Acknowledged {
+		err = errors.New("elasticsearch did not acklowledge new index")
+	}
+	return err
+}
+
+// DeleteIndex deletes the index specified in the struct.
+func (s *Index) DeleteIndex(index string) error {
+	deleteIndex, err := s.cl.conn.DeleteIndex(index).Do(context.TODO())
+	if err == nil && !deleteIndex.Acknowledged {
+		err = errors.New("elasticsearch did not acklowledge deletion of index")
+	}
+	return err
+}
+
+func (s *Index) PutIndexTemplate(name string, body string) error {
+	res, err := s.cl.conn.IndexPutTemplate(name).BodyString(body).Do(context.TODO())
+	if err == nil && !res.Acknowledged {
+		err = errors.New("elasticsearch did not acklowledge creation of template")
+	}
+	return err
+}
+
+func (s *Index) DeleteIndexTemplate(name string) error {
+	res, err := s.cl.conn.IndexDeleteTemplate(name).Do(context.TODO())
+	if err == nil && !res.Acknowledged {
+		err = errors.New("elasticsearch did not acklowledge deletion of tempate")
+	}
+	return err
+}
+
+func NewDocType(index *Index, name string, mapping string) *DocType {
+	return &DocType{
+		Index:   index,
+		name:    name,
+		mapping: mapping,
+	}
+}
+
+type DocType struct {
+	*Index
+	name    string
+	mapping string
+}
+
+// IndexDoc creates a document in elasticsearch
+func (s *DocType) IndexDoc(doc interface{}, id string) (string, error) {
+	q := s.cl.conn.Index().Index(s.Index.name).Type(s.name).BodyJson(doc)
+	if id != "" {
+		q = q.Id(id)
+	}
+
+	res, err := q.Do(context.TODO())
+	if err != nil {
+		return "", err
+	}
+	return res.Id, err
+}
+
+// Get retrieves a document from elasticsearch by id
+func (s *DocType) Get(id string) (*elastic.GetResult, error) {
+	res, err := s.cl.conn.Get().Index(s.Index.name).Type(s.name).Id(id).Do(context.TODO())
+	return res, err
+}
+
+// Delete removes one document from elasticsearch by id
+func (s *DocType) Delete(id string) (bool, error) {
+	res, err := s.cl.conn.Delete().Index(s.Index.name).Type(s.name).Id(id).Do(context.TODO())
+	return res.Found, err
+}
+
+// Search takes a json search string and executes it, returning the result
+func (s *DocType) Search(json interface{}) (*elastic.SearchResult, error) {
+	return s.cl.conn.Search(s.Index.name).Source(json).Pretty(true).Do(context.TODO())
+}
---
+++ elastic_test.go
@@ -0,0 +1,218 @@
+package elasticsearch
+
+import (
+	"log"
+	"testing"
+)
+
+var indicesTests = []struct {
+	index    string
+	expected bool
+}{
+	{"unit_1", true},
+	{"unit_2", true},
+	{"unit_3", true},
+}
+
+func TestIndexExists(t *testing.T) {
+	RegisterClient("local", "http://127.0.0.1:9200")
+
+	//el := New("unit_test", "", "", "")
+	for _, tt := range indicesTests {
+		ind := NewIndex(tt.index, "local")
+
+		if exists, err := ind.indexExists(tt.index); err != nil {
+			log.Print("err")
+			t.Error(err)
+		} else if exists {
+			log.Print("came here")
+			if err := ind.DeleteIndex(tt.index); err != nil {
+				t.Error(err)
+			}
+		}
+	}
+}
+
+func TestCreateIndex(t *testing.T) {
+	ind := NewIndex("", "local")
+	for _, tt := range indicesTests {
+		if err := ind.CreateIndex(tt.index); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestDeleteIndex(t *testing.T) {
+	el := NewIndex("", "local")
+	for _, tt := range indicesTests {
+		if err := el.DeleteIndex(tt.index); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+var indexingTests = []struct {
+	doc      interface{}
+	id       string
+	expected string
+}{
+	{`{"test": "bla"}`, "1", "1"},
+	{`{"test": "blubb"}`, "2", "2"},
+	{`{"test": "bling"}`, "asf2", "asf2"},
+}
+
+func TestIndex(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	doc := NewDocType(ind, "test", ``)
+	for _, tt := range indexingTests {
+		if actual, err := doc.IndexDoc(tt.doc, tt.id); err != nil {
+			t.Error(err)
+		} else if actual != tt.expected {
+			t.Errorf("Fib(%s): expected %s, actual %s", tt.doc, tt.expected, actual)
+		}
+	}
+}
+
+var getTests = []struct {
+	doc interface{}
+	id  string
+}{
+	{`{"test": "bla"}`, "1"},
+	{`{"test": "blubb"}`, "2"},
+	{`{"test": "bling"}`, "asf2"},
+}
+
+func TestGet(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	doc := NewDocType(ind, "test", ``)
+	for _, tt := range getTests {
+		if actual, err := doc.Get(tt.id); err != nil {
+			t.Error(err)
+		} else if string(*actual.Source) != tt.doc {
+			t.Error(actual, tt.doc)
+		}
+	}
+}
+
+var searchTests = []struct {
+	json     string
+	expected int64
+}{
+	{`{
+			"query": {
+        "bool": {
+          "must": {
+            "match": {"test": "bla"}
+          }
+        }
+    	}
+		}`, 1},
+}
+
+func TestSearch(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	doc := NewDocType(ind, "test", ``)
+	for _, tt := range searchTests {
+		if actual, err := doc.Search(tt.json); err != nil {
+			t.Error(err)
+		} else if actual.TotalHits() != tt.expected {
+			t.Error(actual, tt.expected)
+		}
+	}
+}
+
+var deleteTests = []struct {
+	id       string
+	expected bool
+}{
+	{"2", true},
+	{"asf2", true},
+}
+
+func TestDelete(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	doc := NewDocType(ind, "test", ``)
+	for _, tt := range deleteTests {
+		if found, err := doc.Delete(tt.id); err != nil {
+			t.Error(err)
+		} else if found != tt.expected {
+			t.Error(found, tt.expected)
+		}
+	}
+}
+
+var indexTemplateTests = []struct {
+	name         string
+	templateBody string
+}{
+	{"templ1", `{
+		"template": "te*",
+		"settings" : {
+			"number_of_shards" : 1
+		},
+		"mappings" : {
+			"type1" : {
+				"_source" : { "enabled" : false }
+			}
+		}
+	}`},
+}
+
+func TestPutIndexTemplate(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	for _, tt := range indexTemplateTests {
+		if err := ind.PutIndexTemplate(tt.name, tt.templateBody); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+func TestDeleteIndexTemplate(t *testing.T) {
+	ind := NewIndex("unit_test", "local")
+	for _, tt := range indexTemplateTests {
+		if err := ind.DeleteIndexTemplate(tt.name); err != nil {
+			t.Error(err)
+		}
+	}
+}
+
+// func Test1(t *testing.T) {
+// 	conn.PutIndexTemplate("rrmail_template", `{
+// 		"template" : "rrmail-*",
+// 		"settings" : {
+// 			"analysis": {
+// 				"analyzer": {
+// 					"html_analyzer": {
+// 						"tokenizer":     "standard",
+// 						"char_filter": [ "html_strip" ]
+// 					}
+// 				}
+// 			}
+// 		},
+// 		"mappings" : {
+// 			"mail": {
+// 				"properties": {
+// 					"attachmentCount": {"type": "integer"},
+// 					"bcc": {"type": "string"},
+// 					"cc": {"type": "string"},
+// 					"contentType": {"type": "string", "index" : "not_analyzed"},
+// 					"flaghashcode": {"type": "long"},
+// 					"flags": {"type": "string", "index" : "not_analyzed"},
+// 					"folderFullName": {"type": "string", "index" : "not_analyzed"},
+// 					"folderUri": {"type": "string", "index" : "not_analyzed"},
+// 					"from.email": {"type": "string"},
+// 					"from.personal": {"type": "string"},
+// 					"htmlContent": {"type": "string", "index" : "analyzed", "analyzer" : "html_analyzer"},
+// 					"mailboxType": {"type": "string", "index" : "not_analyzed"},
+// 					"receivedDate": {"type": "date", "ignore_malformed": "true"},
+// 					"sendDate": {"type": "date", "ignore_malformed": "true"},
+// 					"size": {"type": "long"},
+// 					"subject": {"type": "string"},
+// 					"textContent": {"type": "string"},
+// 					"to": {"type": "object"},
+// 					"uid": {"type": "long"}
+// 				}
+// 			}
+// 		}
+// 	}`)
+// }
